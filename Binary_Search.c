be76b36a2d010c5f3ac5e965b7160c6329c12c2b
Shuvo <106487378+ashikshuvo166818@users.noreply.github.com>
2023-09-08 21:26:35 +0600

    Update Binary_Search.c

--- Binary_Search.c
+++ Binary_Search.c
@@ -1,33 +1,39 @@
 #include <stdio.h>
+
 int main() {
-  int a;
-  printf("Enter array size : ");
-  scanf("%d", &a);
-  int ash[a], b, c, d, e, f, g, h, i, j = 0, m;
-  printf("Enter array Elements : ");
-  for (b = 0; b < a; b++) {
-    scanf("%d", &ash[b]);
-  }
-  printf("Enter searching Elements : ");
-  scanf("%d", &e);
-  m = 0;
-  for (f = 0; f < a; f++) {
-    g = 0;
-    h = a - 1;
-    i = (g + h) / 2;
-    if (e == ash[f]) {
-      printf("%d's position is %d\n", e, f + 1);
-      return 0;
-    } else if (ash[f] < e) {
-      g = i;
-      h = a - 1;
-    } else if (e < ash[f]) {
-      g = 0;
-      h = i;
-    } else {
-      m++;
+    int a;
+    printf("Enter array size : ");
+    scanf("%d", &a);
+
+    int ash[a], b, e;
+    printf("Enter array Elements (in sorted order): ");
+    for (b = 0; b < a; b++) {
+        scanf("%d", &ash[b]);
     }
-  }
-    printf("Not found\n");
-  return 0;
+
+    printf("Enter searching Element : ");
+    scanf("%d", &e);
+
+    int low = 0, high = a - 1;
+    int found = 0;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (ash[mid] == e) {
+            printf("%d's position is %d\n", e, mid + 1);
+            found = 1;
+            break;
+        } else if (ash[mid] < e) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    if (!found) {
+        printf("Not found\n");
+    }
+
+    return 0;
 }
